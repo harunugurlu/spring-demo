96be3dc7139dad5c906de68dc76bcedc0b23cf64
harunugurlu <harunugurlu1@gmail.com>
2023-08-26 17:05:51 +0300

    add request param to hello controller

--- src/main/java/org/spring/demo/HelloController.java
+++ src/main/java/org/spring/demo/HelloController.java
@@ -2,15 +2,20 @@ package org.spring.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
-    public String printHelloWorld(ModelMap modelMap, String message) {
-        modelMap.addAttribute("Hello World, my first Spring MVC project.");
+    public String printHelloWorld(ModelMap modelMap, @RequestParam(value="message", required=false) String message) {
+        if(message != null)
+            modelMap.addAttribute("message", message);
+        else
+            modelMap.addAttribute("message", "Hello World, my first Spring MVC project.");
 
         return "hello_world";
     }
