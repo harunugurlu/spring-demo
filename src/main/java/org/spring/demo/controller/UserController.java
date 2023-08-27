14a19e7fbe08c3b445d128ebdb398807288e228a
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 16:06:54 +0300

    add create user endpoint

--- src/main/java/org/spring/demo/controller/UserController.java
+++ src/main/java/org/spring/demo/controller/UserController.java
@@ -1,4 +1,52 @@
 package org.spring.demo.controller;
 
+import org.spring.demo.model.Store;
+import org.spring.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+    private Store store = new Store();
+
+    /*@RequestMapping(value = "/create-user", method = RequestMethod.GET)
+    public String user() {
+        return "create-user";
+    }*/
+    @RequestMapping(value = "/create-user", method = RequestMethod.GET)
+    public String createUser(ModelMap modelMap, @RequestParam String name, @RequestParam String email, @RequestParam String address) {
+
+        User existingUser = isValidUser(name);
+
+        if(existingUser == null) {
+            User newUser = new User(name, email, address);
+
+            store.store(newUser);
+
+            String message = "SUCCESSFULLY CREATED USER: " + name;
+            modelMap.addAttribute("message", message);
+
+            modelMap.addAttribute("name", name);
+            modelMap.addAttribute("email", email);
+            modelMap.addAttribute("address", address);
+        }
+        else {
+            String message = "USER: " + name + " ALREADY EXISTS";
+            modelMap.addAttribute("message", message);
+
+            modelMap.addAttribute("name", existingUser.getName());
+            modelMap.addAttribute("email", existingUser.getEmail());
+            modelMap.addAttribute("address", existingUser.getAddress());
+        }
+
+        return "create_user";
+    }
+
+    private User isValidUser(String name) {
+        User existingUser = store.getUser(name);
+        return existingUser;
+    }
 }
