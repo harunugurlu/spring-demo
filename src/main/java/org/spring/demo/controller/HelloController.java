e8aba0d28bb0ab81682ff19fc666f969994c1ec2
harunugurlu <harunugurlu1@gmail.com>
2023-08-27 15:30:30 +0300

    replace controllers

--- src/main/java/org/spring/demo/controller/HelloController.java
+++ src/main/java/org/spring/demo/controller/HelloController.java
@@ -1,8 +1,7 @@
-package org.spring.demo;
+package org.spring.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
